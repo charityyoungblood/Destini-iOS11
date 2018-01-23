0c812d337613b6d9a94cc2f7683155e949dbf49e
Charity <Youngblood.charity@gmail.com>
2018-01-23 13:19:51 -0800

    created and organized files by model, view, controller

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     // connect top button with "a" answers
     // connect bottom button with "b" answers
     // depending on if user selects answer a or answer b you will display corresponding story number
-    // stories do not go sequentially - the user's answer will determine the next story number to display 
+    // stories do not go sequentially - the user's answer will determine the next story number to display
+    // note which selections go to which story numbers
+    // story1 is first, at start of the game
     // i.e. story1 is displayed > user selects answer1b > story
 
     // Our strings
@@ -21,19 +23,26 @@ class ViewController: UIViewController {
     let answer1a = "I\'ll hop in. Thanks for the help!"
     let answer1b = "Better ask him if he\'s a murderer first."
     
+    // if user selected answer1a > display story3
+    // if user selected answer1b > display story2
     
     let story2 = "He nods slowly, unphased by the question."
     let answer2a = "At least he\'s honest. I\'ll climb in."
     let answer2b = "Wait, I know how to change a tire."
     
+    // if user selected answer2a > display story3
+    // if user selected answer2b > display story4
+    
     let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
     let answer3a = "I love Elton John! Hand him the cassette tape."
     let answer3b = "It\'s him or me! You take the knife and stab him."
     
+    // if user selected answer3a > display story6
+    // if user selected answer3b > display story5
+    
     let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
     let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
     let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
-    
     
     // UI Elements linked to the storyboard
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
