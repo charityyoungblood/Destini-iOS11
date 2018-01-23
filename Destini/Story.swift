62ca58d48731ff36aa3fd651b13fa28004f4ba4a
Charity <Youngblood.charity@gmail.com>
2018-01-23 13:25:54 -0800

    moved story data to StoryQuestions class/folder

---
+++ Destini/Story.swift
@@ -0,0 +1,13 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Charity Youngblood on 1/23/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Story {
+    
+}
--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -18,31 +18,6 @@ class ViewController: UIViewController {
     // story1 is first, at start of the game
     // i.e. story1 is displayed > user selects answer1b > story
 
-    // Our strings
-    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
-    let answer1a = "I\'ll hop in. Thanks for the help!"
-    let answer1b = "Better ask him if he\'s a murderer first."
-    
-    // if user selected answer1a > display story3
-    // if user selected answer1b > display story2
-    
-    let story2 = "He nods slowly, unphased by the question."
-    let answer2a = "At least he\'s honest. I\'ll climb in."
-    let answer2b = "Wait, I know how to change a tire."
-    
-    // if user selected answer2a > display story3
-    // if user selected answer2b > display story4
-    
-    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
-    let answer3a = "I love Elton John! Hand him the cassette tape."
-    let answer3b = "It\'s him or me! You take the knife and stab him."
-    
-    // if user selected answer3a > display story6
-    // if user selected answer3b > display story5
-    
-    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
-    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
-    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
     
     // UI Elements linked to the storyboard
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
@@ -50,9 +25,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyTextView: UILabel!
     
     // TODO Step 5: Initialise instance variables here
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
